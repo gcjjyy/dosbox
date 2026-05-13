63bc7b0c5eb282974486b46b65738c62754a1e6e
Cheol Jang <gcjjyy@gmail.com>
2026-05-13 14:56:33 +0900

    feat(dos.jsdos): stream .jsdos bundle, 304 on If-None-Match

--- app/routes/dos.jsdos.tsx
+++ app/routes/dos.jsdos.tsx
@@ -1,3 +1,19 @@
-export async function loader() {
-  return new Response("not implemented", { status: 501 });
+import type { Route } from "./+types/dos.jsdos";
+import { streamJsdosBundle, getBundleEtag } from "../lib/bundle";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const etag = await getBundleEtag();
+  if (request.headers.get("if-none-match") === etag) {
+    return new Response(null, { status: 304, headers: { ETag: etag } });
+  }
+  const { body, etag: bodyEtag } = await streamJsdosBundle();
+  return new Response(body, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "Content-Disposition": 'inline; filename="dos.jsdos"',
+      ETag: bodyEtag,
+      "Cache-Control": "no-cache, must-revalidate",
+    },
+  });
 }
